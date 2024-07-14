e6500614332a5e10f717811632f4082221779efc
Douglas Butner <dougbutner@gmail.com>
2024-07-14 08:17:07 -0500

    Bundled Wharfkit

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import { SessionKit } from "@wharfkit/session"
 import { WebRenderer } from "@wharfkit/web-renderer"
 import { WalletPluginCloudWallet } from "@wharfkit/wallet-plugin-cloudwallet"
 import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor"
-import { WalletPluginWombat } from "@wharfkit/wallet-plugin-wombat"
+//import { WalletPluginWombat } from "@wharfkit/wallet-plugin-wombat"
 
 async function main() {
   window.Wharfkit = {
     SessionKit,
     WebRenderer,
     WalletPluginCloudWallet,
-    WalletPluginAnchor,
-    WalletPluginWombat
+    WalletPluginAnchor//,
+   // WalletPluginWombat
   }
 }
 
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@
 export default {
   build: {
     // Set the entry point for your application
+    target: 'esnext',
     rollupOptions: {
       input: {
         main: 'index.js' // Assuming your entry file is named index.js
+      },
+      output: {
+        // Ensuring the output format is IIFE
+        format: 'iife',
       }
+
     }
   }
 }
